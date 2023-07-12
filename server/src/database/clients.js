d93f87029071c244e7d6cdd4730a221b3b125b15
Rafael <rafael.pinho@aluno.ufop.edu.br>
2023-07-12 20:25:35 -0300

    bd connection

--- server/src/database/clients.js
+++ server/src/database/clients.js
@@ -1,5 +1,13 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+var mysql = require('mysql');
 
+var con = mysql.createConnection({
+  host: "br686.hostgator.com.br",
+  user: "donac936_rafael",
+  password: "Brasil2014",
+  database: "donac936_testeRafael"
+});
 
-export { prisma }
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
